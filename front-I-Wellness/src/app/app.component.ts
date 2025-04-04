1f8ae21ece74eb93e58d6d157611e3b9ebb32087
clarck2547 <63880026+clarck2547@users.noreply.github.com>
2025-04-04 17:21:43 -0500

    reorganizacion de carpetas (#6)

--- front-I-Wellness/src/app/app.component.ts
+++ front-I-Wellness/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import  { CommonModule } from '@angular/common';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { HeaderAdminComponent } from './components/users/administrador/header-admin/header-admin.component';
-import { HeaderProveedorComponent } from './components/users/proveedor/header-proveedor/header-proveedor.component';
-import { HeaderTuristaComponent } from './components/users/turista/header-turista/header-turista.component';
+import { HeaderAdminComponent } from './features/users/administrador/header-admin/header-admin.component';
+import { HeaderProveedorComponent } from './features/users/proveedor/header-proveedor/header-proveedor.component';
+import { HeaderTuristaComponent } from './features/users/turista/header-turista/header-turista.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+
 
 
 
